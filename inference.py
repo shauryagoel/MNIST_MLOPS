35fca541a8a8f4bc1741151a8562056708370251
Shaurya Goel <shaurya.goel@awiros.com>
2023-01-28 13:31:34 +0000

    move onnxruntime related functions to utility script to make them reusable

--- inference.py
+++ inference.py
@@ -4,38 +4,26 @@ from pathlib import Path
 
 import cv2
 import numpy as np
-import onnxruntime
 
-from utils import load_yaml_config
+import utils
 
 # Set logging level to info
 logging.basicConfig(level=logging.INFO)
 
 # Load config
-config = load_yaml_config(Path("inference.yaml"))
+config = utils.load_yaml_config(Path("inference.yaml"))
 onnx_model_path = config["onnx_model_path"]
 image_path = config["image_path"]
 
 # Load the onnx model
-ort_session = onnxruntime.InferenceSession(
-    onnx_model_path, providers=["CPUExecutionProvider"]
-)
+ort_session = utils.load_onnx_model(Path(onnx_model_path))
 
 # Load the image and preprocess it such that it can be passed to the model
-img = cv2.imread(image_path)
-img = cv2.resize(img, (28, 28))
-# convert from RGB to grayscale as model takes grayscale images
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-x = img.astype(np.float32)
-x = x[None]
-x = x.reshape(1, 28 * 28)
-x = np.ascontiguousarray(x)
-
+image = cv2.imread(image_path)
+pre_processed_image = utils.pre_process_image(image)
 
 # Do actual onnxruntime inference
-ort_inputs = {ort_session.get_inputs()[0].name: x}
-ort_outs = ort_session.run(None, ort_inputs)
-
+ort_outs = utils.ort_inference(ort_session, pre_processed_image)
 
 # Take the max index of predictions
 predicted_digit = np.argmax(ort_outs[0])
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 # Hold some basic utility functions
 from pathlib import Path
 
+import cv2
+import numpy as np
+import onnxruntime
 import yaml
 
 
@@ -9,3 +12,38 @@ def load_yaml_config(file_path: Path):
     with open(file_path, "r") as f:
         config = yaml.load(f, yaml.Loader)
     return config
+
+
+def load_onnx_model(onnx_model_file_path: Path):
+    """Load the onnx model from the file path on CPU."""
+    ort_session = onnxruntime.InferenceSession(
+        str(onnx_model_file_path), providers=["CPUExecutionProvider"]
+    )
+    print(type(ort_session))
+    return ort_session
+
+
+def pre_process_image(image: np.ndarray):
+    """Preprocess the image according to the trained model.
+    Args:
+        image: numpy array which represents image in BGR format
+    """
+    x = cv2.resize(image, (28, 28))
+    # Convert from RGB to grayscale as model takes grayscale images
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+    x = x.astype(np.float32)
+    x = x[None]
+    x = x.reshape(1, 28 * 28)
+    x = np.ascontiguousarray(x)
+    return x
+
+
+def ort_inference(onnx_model, image: np.ndarray):
+    """Do onnx inference on image using onnx_model.
+    Args:
+        onnx_model: onnxruntime session denoting the model
+        image: numpy array which has shape (1, 784) and data type float32
+    """
+    ort_inputs = {onnx_model.get_inputs()[0].name: image}
+    ort_outs = onnx_model.run(None, ort_inputs)
+    return ort_outs
